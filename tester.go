d4801d459d67af55d4c6a2986e64660839460f60
Brian Ward <bward@redhat.com>
2019-06-18 08:54:22 -0400

    WIP building blocks and plans.

---
+++ tester.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "net/http"
+import "log"
+
+func main() {
+	// TODO: get HTTP Server pods available in namespace
+	// curl route endpoint with cookies set, in loop
+	// capture results, confirm cookie being used or not
+}
